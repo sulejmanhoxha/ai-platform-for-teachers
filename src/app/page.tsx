68e599edc14ee19ffee78f7c1c809baf33cbf0bb
Sulejman Hoxha <99233924+sulejmanhoxha@users.noreply.github.com>
2024-12-08 02:04:51 +0100

    Initialize better-auth

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello World!</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "sqlite", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+});
