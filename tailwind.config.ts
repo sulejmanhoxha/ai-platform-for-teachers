9c6084d0352c3c90cfc6a1976ad51f42c2fd2a86
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2024-12-17 12:45:34 +0100

    feat: install openai sdk, react-markdown and tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,5 +71,6 @@ export default {
   plugins: [
     require("tailwindcss-animate"),
     require("@tailwindcss/container-queries"),
+    require("@tailwindcss/typography"),
   ],
 } satisfies Config;
