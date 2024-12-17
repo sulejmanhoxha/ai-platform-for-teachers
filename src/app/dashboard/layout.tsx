c8d116d65942e8eb5ea722627b1412e33f831be4
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2024-12-17 12:47:55 +0100

    style

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -25,7 +25,7 @@ export default function DashboardLayout({
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          <div className="@container min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
             {children}
           </div>
         </div>
